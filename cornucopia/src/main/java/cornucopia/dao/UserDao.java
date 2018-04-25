f1306f2e7dd8214fb47c90c9d362745611f19543
shuqizhao <shuqizhao@beisen.com>
2018-04-25 21:50:01 +0800

    login condition

--- cornucopia/src/main/java/cornucopia/dao/UserDao.java
+++ cornucopia/src/main/java/cornucopia/dao/UserDao.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Select;
 import cornucopia.model.UserEntity;
 
 public interface UserDao {
-	@Select("select * from sys_user where name=#{un} and pwd=#{pwd}")
+	@Select("select * from sys_user where name=#{un} and pwd=#{pwd} and is_deleted=0 and is_enabled=1")
     public UserEntity getUserEntity(@Param("un") String un, @Param("pwd") String pwd);
 }
