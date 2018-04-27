1dec52773ac2b2f0bd4aff5536f4383f7ba485e5
shuqizhao <shuqizhao@qq.com>
2018-04-27 14:50:36 +0800

    swagger

--- cornucopia/src/main/java/cornucopia/util/CornucopiaConfig.java
+++ cornucopia/src/main/java/cornucopia/util/CornucopiaConfig.java
@@ -3,10 +3,11 @@ package cornucopia.util;
 import org.springframework.context.annotation.Configuration;  
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration  
-public class CorsConfig extends WebMvcConfigurationSupport  {  
+public class CornucopiaConfig extends WebMvcConfigurationSupport  {  
 
 	/**
      * 解决跨域问题
@@ -27,4 +28,17 @@ public class CorsConfig extends WebMvcConfigurationSupport  {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor());
     }
+    
+    /**
+     * swagger支持
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }  
