7e9199c9390814ad6addb60784c01ed04db6cd7c
shuqizhao <shuqizhao@qq.com>
2018-05-18 18:47:59 +0800

    new task

---
+++ cornucopia.static/src/biz/departure/apply.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+  data() {
+    var self = this;
+    return {};
+  }
+};
+</script>
+<style>
+
+</style>
+
---
+++ cornucopia.static/src/biz/newTask/apply.vue
@@ -0,0 +1,96 @@
+<template>
+     <div>
+         <div class="box box-default">
+            <div class="box-header with-border">
+              <i class="fa fa-money"></i>
+
+              <h3 class="box-title">财务类</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 费用报销
+              </a>
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 借款申请
+              </a>
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 出差申请
+              </a>
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 差旅报销
+              </a>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+          <div class="box box-default">
+            <div class="box-header with-border">
+              <i class="fa  fa-paperclip"></i>
+
+              <h3 class="box-title">行政类</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 用印申请
+              </a>
+               <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 合同审批
+              </a>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+          <div class="box box-default">
+            <div class="box-header with-border">
+              <i class="fa fa-odnoklassniki"></i>
+
+              <h3 class="box-title">人事类</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 录用审批
+              </a>
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 入职申请
+              </a>
+               <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 离职申请
+              </a>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+          <div class="box box-default">
+            <div class="box-header with-border">
+              <i class="fa fa-shopping-cart"></i>
+
+              <h3 class="box-title">采购类</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <a class="btn btn-app">
+                <i class="fa fa-plus-circle"></i> 采购申请
+              </a>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+     </div>
+</template>
+<script>
+export default {
+  data() {
+    var self = this;
+    return {};
+  }
+};
+</script>
+<style>
+</style>
+
