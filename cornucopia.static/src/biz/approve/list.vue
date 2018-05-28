13828a80f3c68300e585956db830082f2bc73e40
shuqizhao <shuqizhao@qq.com>
2018-05-28 11:49:40 +0800

    approve

--- cornucopia.static/src/biz/approve/list.vue
+++ cornucopia.static/src/biz/approve/list.vue
@@ -1,6 +1,7 @@
 <template>
   <el-row>
-    <el-col :span="12"><list :cfg="cfg"></list></el-col>
+    <el-col :span="6"><tree  ref="tree" :cfg="treeCfg"></tree></el-col>
+    <el-col :span="6"><tree  ref="tree" :cfg="treeCfg1"></tree></el-col>
     <el-col :span="12"><list :cfg="cfg1"></list></el-col>
   </el-row>
 </template>
@@ -9,109 +10,145 @@ export default {
   data() {
     var self = this;
     return {
-      cfg: {
-        title: "审批路径",
+      treeCfg: {
+        title: "审批路线(一)",
         parentTitle: "流程管理",
-        simpleUrl: this.getGlobalData().ApiBaseUrl + "/processnode/alllist",
-        lengthMenu: [[-1], ["ALL"]],
-        sDom: '<"dataTables_function"/>',
-        bServerSide: false,
-        hideCheckBox: true,
-        showSelectedRowColor: true,
-        isShowSearchArea:"true",
-        columns: [
+        url: this.getGlobalData().ApiBaseUrl + "/auth/allResource",
+        functions: [
           {
-            title: "id",
-            name: "id",
-            isHide: true
-          },
-          {
-            title: "名称",
-            name: "name",
-            isSearch: true,
-            type: "combox",
-            data: [
-              {
-                id: "2",
-                value: "离职流程"
-              },
-              {
-                id: "1",
-                value: "是"
-              },
-              {
-                id: "0",
-                value: "否"
-              }
-            ]
-          },
-          {
-            title: "是否启用",
-            name: "isEnabled"
-          },
-          {
-            title: "创建时间",
-            name: "createTime"
-          }
-        ],
-        idName: "id",
-        fnRowCallback: function(row, data) {
-          if (data.isEnabled) {
-            $("td:eq(1)", row).html('<i class="fa fa-fw fa-check-circle"></i>');
-          } else {
-            $("td:eq(1)", row).html('<i class="el-icon-close"></i>');
-          }
-        },
-        functions: {
-          common: [
-            {
-              text: "添加路径",
-              url: "processAdd",
-              mode: "modal",
-              functionName: "processAdd"
-            }
-          ],
-          more: [
-            {
-              text: "停用",
-              url: this.getGlobalData().ApiBaseUrl + "/processnode/disable"
-            },
-            {
-              text: "启用",
-              url: this.getGlobalData().ApiBaseUrl + "/processnode/enable"
-            },
-            {
-              text: "删除",
-              url: this.getGlobalData().ApiBaseUrl + "/processnode/delete"
-            }
-          ]
-        },
-        onClickRow: function(data, target) {
-          self.$refs.tree.cfg.title = data.name;
-          self.openLoading($("#resTree")[0]);
-          $.ajax({
-            type: "GET",
-            xhrFields: {
-              withCredentials: true
-            },
-            url:
-              self.getGlobalData().ApiBaseUrl +
-              "/auth/getCheckedList?roleId=" +
-              data.id,
-            success: function(response) {
-              if (response.code == "200") {
-                self.$refs.tree.setCheckedKeys(response.data);
-                self.closeLoading();
-                self.currentRoleId = data.id;
-              } else if (response.message) {
+            text: "保存",
+            type: "btn-success",
+            icon: "el-icon-success",
+            onClick: function() {
+              if (!self.currentRoleId) {
                 self.$message({
-                  type: "warning",
-                  message: response.message
+                  message: "请先选择角色!",
+                  type: "warning"
                 });
+                return;
               }
+              self
+                .$confirm("确定要保存吗", "提示", {
+                  confirmButtonText: "确定",
+                  cancelButtonText: "取消",
+                  type: "info"
+                })
+                .then(() => {
+                  $.ajax({
+                    type: "POST",
+                    xhrFields: {
+                      withCredentials: true
+                    },
+                    data: {
+                      roleId: self.currentRoleId,
+                      checkedList: self.$refs.tree.getCheckedKeys()
+                    },
+                    url:
+                      self.getGlobalData().ApiBaseUrl + "/auth/saveCheckedList",
+                    success: function(response) {
+                      if (response.code == "200") {
+                        self.$message({
+                          message: "操作成功!",
+                          type: "success"
+                        });
+                      } else if (response.message) {
+                        self.$message({
+                          type: "warning",
+                          message: response.message
+                        });
+                      }
+                    }
+                  });
+                });
             }
-          });
-        }
+          }
+          // {
+          //   text: "全选",
+          //   type: "btn-success",
+          //   icon:'el-icon-circle-check-outline',
+          //   onClick:function(){
+          //     self.$refs.tree.checkAll();
+          //   }
+          // },
+          // {
+          //   text: "清空",
+          //   type: "btn-success",
+          //   icon:'el-icon-circle-close-outline',
+          //   onClick:function(){
+          //     self.$refs.tree.clearAll();
+          //   }
+          // }
+        ]
+      },
+       treeCfg1: {
+        title: "审批路线(二)",
+        parentTitle: "流程管理",
+        url: this.getGlobalData().ApiBaseUrl + "/auth/allResource",
+        functions: [
+          {
+            text: "保存",
+            type: "btn-success",
+            icon: "el-icon-success",
+            onClick: function() {
+              if (!self.currentRoleId) {
+                self.$message({
+                  message: "请先选择角色!",
+                  type: "warning"
+                });
+                return;
+              }
+              self
+                .$confirm("确定要保存吗", "提示", {
+                  confirmButtonText: "确定",
+                  cancelButtonText: "取消",
+                  type: "info"
+                })
+                .then(() => {
+                  $.ajax({
+                    type: "POST",
+                    xhrFields: {
+                      withCredentials: true
+                    },
+                    data: {
+                      roleId: self.currentRoleId,
+                      checkedList: self.$refs.tree.getCheckedKeys()
+                    },
+                    url:
+                      self.getGlobalData().ApiBaseUrl + "/auth/saveCheckedList",
+                    success: function(response) {
+                      if (response.code == "200") {
+                        self.$message({
+                          message: "操作成功!",
+                          type: "success"
+                        });
+                      } else if (response.message) {
+                        self.$message({
+                          type: "warning",
+                          message: response.message
+                        });
+                      }
+                    }
+                  });
+                });
+            }
+          }
+          // {
+          //   text: "全选",
+          //   type: "btn-success",
+          //   icon:'el-icon-circle-check-outline',
+          //   onClick:function(){
+          //     self.$refs.tree.checkAll();
+          //   }
+          // },
+          // {
+          //   text: "清空",
+          //   type: "btn-success",
+          //   icon:'el-icon-circle-close-outline',
+          //   onClick:function(){
+          //     self.$refs.tree.clearAll();
+          //   }
+          // }
+        ]
       },
       cfg1: {
         title: "审批岗位",
