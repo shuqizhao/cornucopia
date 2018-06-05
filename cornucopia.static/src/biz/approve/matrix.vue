ad5831b2183e60afe48fd6b722b22cacc187580c
shuqizhao <shuqizhao@qq.com>
2018-06-05 11:41:13 +0800

    page add

---
+++ cornucopia.static/src/biz/approve/matrix.vue
@@ -0,0 +1,97 @@
+<template>
+  <list :cfg="cfg"></list>
+</template>
+<script>
+export default {
+  data() {
+    var self = this;
+    return {
+      cfg: {
+        title: "审批矩阵",
+        parentTitle: "权限管理",
+        simpleUrl: this.getGlobalData().ApiBaseUrl + "/approve/matrix",
+        lengthMenu: [[-1], ["ALL"]],
+        sDom: 'f<"dataTables_function"/>',
+        bServerSide: false,
+        hideCheckBox: true,
+        showSelectedRowColor: true,
+        columns: [
+          {
+            title: "id",
+            name: "id",
+            isHide: true
+          },
+          {
+            title: "函数名",
+            name: "name"
+          },
+          {
+            title: "函数描述",
+            name: "desc"
+          },
+          {
+            title: "是否启用",
+            name: "isEnabled"
+          },
+          {
+            title: "创建时间",
+            name: "createTime"
+          },
+          {
+            title: "修改时间",
+            name: "updateTime"
+          }
+        ],
+        idName: "id",
+        fnRowCallback: function(row, data) {
+          if (data.isEnabled) {
+            $("td:eq(2)", row).html('<i class="fa fa-fw fa-check-circle"></i>');
+          } else {
+            $("td:eq(2)", row).html('<i class="el-icon-close"></i>');
+          }
+        },
+        functions: {
+          common: [
+            {
+              text: "添加函数",
+              url: "functionAdd",
+              mode: "modal"
+            },
+            {
+              text: "编辑函数",
+              url: "functionEdit",
+              mode: "modal"
+            }
+          ],
+          more: [
+            {
+              text: "停用",
+              url: this.getGlobalData().ApiBaseUrl + "/function/disable"
+            },
+            {
+              text: "启用",
+              url: this.getGlobalData().ApiBaseUrl + "/function/enable"
+            },
+            {
+              text: "删除",
+              url: this.getGlobalData().ApiBaseUrl + "/function/delete"
+            }
+          ]
+        },
+        onClickRow: function(data, target) {
+          self.id = data.id;
+        }
+      },
+      id:0
+    };
+  },
+  updated: function() {
+    var self = this;
+  }
+};
+</script>
+<style>
+.dataTables_filter {
+  margin-bottom: -40px;
+}
+</style>
---
+++ cornucopia.static/src/biz/approve/ruleList.vue
@@ -0,0 +1,101 @@
+<template>
+<el-row>
+    <el-col :span="12"><list :cfg="cfg"></list></el-col>
+    <el-col :span="12"><tree  ref="tree1" :cfg="cfg"></tree></el-col>
+  </el-row>
+  
+</template>
+<script>
+export default {
+  data() {
+    var self = this;
+    return {
+      cfg: {
+        title: "函数管理",
+        parentTitle: "权限管理",
+        simpleUrl: this.getGlobalData().ApiBaseUrl + "/function/alllist",
+        lengthMenu: [[-1], ["ALL"]],
+        sDom: 'f<"dataTables_function"/>',
+        bServerSide: false,
+        hideCheckBox: true,
+        showSelectedRowColor: true,
+        columns: [
+          {
+            title: "id",
+            name: "id",
+            isHide: true
+          },
+          {
+            title: "函数名",
+            name: "name"
+          },
+          {
+            title: "函数描述",
+            name: "desc"
+          },
+          {
+            title: "是否启用",
+            name: "isEnabled"
+          },
+          {
+            title: "创建时间",
+            name: "createTime"
+          },
+          {
+            title: "修改时间",
+            name: "updateTime"
+          }
+        ],
+        idName: "id",
+        fnRowCallback: function(row, data) {
+          if (data.isEnabled) {
+            $("td:eq(2)", row).html('<i class="fa fa-fw fa-check-circle"></i>');
+          } else {
+            $("td:eq(2)", row).html('<i class="el-icon-close"></i>');
+          }
+        },
+        functions: {
+          common: [
+            {
+              text: "添加函数",
+              url: "functionAdd",
+              mode: "modal"
+            },
+            {
+              text: "编辑函数",
+              url: "functionEdit",
+              mode: "modal"
+            }
+          ],
+          more: [
+            {
+              text: "停用",
+              url: this.getGlobalData().ApiBaseUrl + "/function/disable"
+            },
+            {
+              text: "启用",
+              url: this.getGlobalData().ApiBaseUrl + "/function/enable"
+            },
+            {
+              text: "删除",
+              url: this.getGlobalData().ApiBaseUrl + "/function/delete"
+            }
+          ]
+        },
+        onClickRow: function(data, target) {
+          self.id = data.id;
+        }
+      },
+      id:0
+    };
+  },
+  updated: function() {
+    var self = this;
+  }
+};
+</script>
+<style>
+.dataTables_filter {
+  margin-bottom: -40px;
+}
+</style>
