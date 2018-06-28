7cf6e94da7962e3d1ead5e4aae42602468dab101
shuqizhao <shuqizhao@qq.com>
2018-06-28 18:53:47 +0800

    deal user

--- cornucopia/src/main/java/cornucopia/service/ProcessService.java
+++ cornucopia/src/main/java/cornucopia/service/ProcessService.java
@@ -1,9 +1,15 @@
 package cornucopia.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
 
 import cornucopia.dao.ProcessDao;
 import cornucopia.entity.ProcessEntity;
+import cornucopia.util.ActivitiHelper;
 import cornucopia.util.MyBatisHelper;
 
 public class ProcessService {
@@ -17,7 +23,7 @@ public class ProcessService {
 	}
 
 	ProcessDao processDao = MyBatisHelper.getMapper(ProcessDao.class);
-	
+
 	public List<ProcessEntity> getAllProcess() {
 		return processDao.getAllProcess();
 	}
@@ -27,17 +33,17 @@ public class ProcessService {
 	}
 
 	public int insert(ProcessEntity processEntity) {
-		 return processDao.insert(processEntity);
+		return processDao.insert(processEntity);
 	}
 
 	public int disable(int id) {
 		return processDao.disable(id);
 	}
-	
+
 	public int enable(int id) {
 		return processDao.enable(id);
 	}
-	
+
 	public int delete(int id) {
 		return processDao.delete(id);
 	}
@@ -45,8 +51,23 @@ public class ProcessService {
 	public ProcessEntity get(String processId) {
 		return processDao.get(processId);
 	}
-	
+
 	public String getPre(String processId) {
 		return get(processId).getPre();
+	}
+
+	public String StartByProcessId(String processId, String userId) {
+		// get from db
+		return Start(processId, userId);
+	}
+
+	public String Start(String key, String userId) {
+		RuntimeService rs = ActivitiHelper.GetEngine().getRuntimeService();
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("dealUser", userId);
+		ProcessInstance pi = rs.startProcessInstanceByKey(key, variables);
+		// String acId = pi.getActivityId();
+		String instId = pi.getProcessInstanceId();
+		return instId;
 	}
 }
