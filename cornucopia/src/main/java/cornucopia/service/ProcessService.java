0cef5dd2578e8c6ee4b585e5bc75c093455044af
shuqizhao <shuqizhao@beisen.com>
2018-07-08 03:57:02 +0800

    log

--- cornucopia/src/main/java/cornucopia/service/ProcessService.java
+++ cornucopia/src/main/java/cornucopia/service/ProcessService.java
@@ -77,9 +77,9 @@ public class ProcessService {
 	public String StartProcess(ProcessDataEntity pde) {
 		Log4jHelper.LOGGER.info(String.format("[%s]->%s->createBy=%d", pde.getFormCode(), "开始流程", pde.getCreateBy()));
 		ProcessDiagramEntity pd = ProcessDiagramService.getInstance().getByProcessId(pde.getProcessId() + "");
-		Log4jHelper.LOGGER.info(String.format("[%s]->defKey=%s", pde.getFormCode(), pd.getDefKey()));
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s->defKey=%s", pde.getFormCode(), "准备启动流程", pd.getDefKey()));
 		String instId = StartProcessByKey(pd.getDefKey(), pde.getCreateBy());
-		Log4jHelper.LOGGER.info(String.format("[%s]->instId=%s", pde.getFormCode(), instId));
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s->instId=%s", pde.getFormCode(), "流程启动成功", instId));
 		pde.setProcinstId(instId);
 		pde.setLevelCount(0);
 		Complete(pde);
@@ -107,29 +107,32 @@ public class ProcessService {
 		variables.put("to", "2");
 		variables.put("dealUser", pde.getCreateBy());
 		variables.put("condition", 0);
-		Log4jHelper.LOGGER.info(String.format("[%s]->taskId=%s", pde.getFormCode(), task.getId()));
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s->taskId=%s", pde.getFormCode(), "查找到引擎任务", task.getId()));
 		ActivitiHelper.GetEngine().getTaskService().complete(task.getId(), variables);
-		Log4jHelper.LOGGER.info(String.format("[%s]->%s", pde.getFormCode(), "引擎退回完成"));
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s", pde.getFormCode(), "引擎任务退回完成"));
 	}
 
 	public void Complete(ProcessDataEntity pde) {
-		Log4jHelper.LOGGER.info(String.format("[%s]->%s->updateBy=%d->instId=%s", pde.getFormCode(), "完成任务",
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s->updateBy=%d->instId=%s", pde.getFormCode(), "准备完成任务",
 				pde.getUpdateBy(), pde.getProcinstId()));
 		List<Integer> nextUserIds = getNextDealUser(pde);
 		if (nextUserIds == null || nextUserIds.size() == 0) {
-			Log4jHelper.LOGGER.error(String.format("[%s]->%s->updateBy=%d->instId=%s", pde.getFormCode(), "找不到人",
+			Log4jHelper.LOGGER.error(String.format("[%s]->%s->updateBy=%d->instId=%s", pde.getFormCode(), "任务找不到人",
 					pde.getUpdateBy(), pde.getProcinstId()));
 			return;
 		}
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s->人员数=%d", pde.getFormCode(), "查找到审核人员", nextUserIds.size()));
 		TaskQuery query = ActivitiHelper.GetEngine().getTaskService().createTaskQuery();
 		query.taskAssignee(pde.getUpdateBy() + "");
 		query.processInstanceId(pde.getProcinstId());
 		Task task = query.singleResult();
-		Log4jHelper.LOGGER.info(String.format("[%s]->taskId=%s", pde.getFormCode(), task.getId()));
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s->taskId=%s", pde.getFormCode(), "查找到引擎任务", task.getId()));
 		Map<String, Object> variables = new HashMap<String, Object>();
 		for (int nextUserId : nextUserIds) {
 			if (nextUserId == 666666) {
 				variables.put("to", "0");
+				Log4jHelper.LOGGER.info(
+						String.format("[%s]->%s->taskId=%s", pde.getFormCode(), "查找到审核人员666666,准备完成DOA", task.getId()));
 			} else {
 				variables.put("to", "1");
 			}
@@ -141,7 +144,7 @@ public class ProcessService {
 	}
 
 	public List<Integer> getNextDealUser(ProcessDataEntity pde) {
-		Log4jHelper.LOGGER.info(String.format("[%s]->%s->updateBy=%d->instId=%s", pde.getFormCode(), "获取审批人员",
+		Log4jHelper.LOGGER.info(String.format("[%s]->%s->updateBy=%d->instId=%s", pde.getFormCode(), "开始获取审批人员",
 				pde.getUpdateBy(), pde.getProcinstId()));
 		TaskQuery query = ActivitiHelper.GetEngine().getTaskService().createTaskQuery();
 		query.processInstanceId(pde.getProcinstId());
