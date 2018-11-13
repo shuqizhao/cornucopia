0ece5f2e9c545338a0e27dd2d064f6d0ee00bf25
shuqizhao <shuqizhao@beisen.com>
2018-11-13 23:36:43 +0800

    rn

--- cornucopia.static/src/biz/process/categoryAdd.vue
+++ cornucopia.static/src/biz/process/categoryAdd.vue
@@ -9,7 +9,7 @@ export default {
       cfg: {
         title: "添加流程",
         mode: "create",
-        save: this.getGlobalData().ApiBaseUrl + "/processCatetory/add",
+        save: this.getGlobalData().ApiBaseUrl + "/processCategory/add",
         items: [
           {
             name: "name",
