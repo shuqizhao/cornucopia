163a0d47e56683e82aeef2b0d86ca2e5b3c23ecb
shuqizhao <shuqizhao@qq.com>
2018-12-10 15:55:57 +0800

    processInstDiagramId

--- cornucopia/src/main/java/cornucopia/dao/ProcessApproveDao.java
+++ cornucopia/src/main/java/cornucopia/dao/ProcessApproveDao.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.SelectKey;
 import cornucopia.entity.ProcessApproveEntity;
 
 public interface ProcessApproveDao {
-    @Insert("call sp_process_approve_insert(#{pd.processId},#{pd.processDataId},#{pd.createBy},#{pd.procinstId},#{pd.levelCount},#{pd.stepName},#{pd.userId})")
+    @Insert("call sp_process_approve_insert(#{pd.processId},#{pd.processDataId},#{pd.createBy},#{pd.procinstId},#{pd.levelCount},#{pd.stepName},#{pd.userId},#{pd.processInstDiagramId})")
 	@SelectKey(statement = "Select LAST_INSERT_ID()", keyProperty = "pd.id", before = false, resultType = int.class)
 	public int insert(@Param("pd") ProcessApproveEntity processApproveEntity);
 }
