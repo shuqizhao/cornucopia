3000426fac0d1797202fc3c51b1ec359e5b7965e
shuqizhao <shuqizhao@qq.com>
2018-12-25 10:03:27 +0800

    rm duoyu

--- cornucopia/src/main/java/cornucopia/dao/ProcessDataDao.java
+++ cornucopia/src/main/java/cornucopia/dao/ProcessDataDao.java
@@ -2,7 +2,6 @@ package cornucopia.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
